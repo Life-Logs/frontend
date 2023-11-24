7f7b34cf2bc2300161ee808126a4b810b8fe9eed
dlwjdals <jml22zzang@gmail.com>
2023-11-24 23:13:58 +0900

    feat: env setup modify

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,51 +1,70 @@
-'use client'
+"use client";
 
-import axios from 'axios'
-import styles from './page.module.css'
+import axios from "axios";
+import styles from "./page.module.css";
+import GoogleLogin from "react-google-login";
+import React, { useState, useEffect, useCallback } from "react";
 
 export default function Home() {
+  const clientIDValue =
+    "383755203832-asb9rbs3f5v1eenk4al5cjlud9ieqim5.apps.googleusercontent.com";
 
-  const GoogleLoginTest = () => {
-    const cookie = document.cookie
-    console.log(cookie)
+  const [isLogin, setIsLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
 
-    axios.get('https://lifelog.devtkim.com/auth/to-google', {
-    // axios.get('https://lifelog.devtkim.com/auth/test-guard2', {
-      headers: {
-        Cookie : cookie
-      }
-    }).then((response) => {
-      console.log(response)
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
+  // const test = () => {
+  //   console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+  //   window.location.href ="https://accounts.google.com/o/oauth2/auth?" +
+  //   "client_id={클라이언트 ID}&"+
+  //   "redirect_uri={리디렉션 URI}&"+
+  //   "response_type=token&"+
+  //   "scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
+  // };
+
+  const googleLogin = useCallback((response) => {
+    const userInfo = {
+      profileImg: response.profileObj.imageUrl,
+      email: response.profileObj.email,
+      name: response.profileObj.name,
+    };
+    setUserInfo(userInfo);
+    setIsLogin(true);
+  }, []);
 
   return (
     <div className={styles.main}>
-      <span className={styles.title}>
-      My Easy Life
-      </span>
+      <span className={styles.title}>My Easy Life</span>
       <div className={styles.email}>
-        <input className={styles.input} placeholder=' 이메일'/>
+        <input className={styles.input} placeholder=" 이메일" />
       </div>
       <div className={styles.passwordCheckBox}>
-        <input className={styles.input} placeholder=' 비밀번호' type='password'/>
+        <input
+          className={styles.input}
+          placeholder=" 비밀번호"
+          type="password"
+        />
         <div className={styles.checkBoxWrapper}>
-          <input className={styles.checkBox} type='checkbox'/> 
+          <input className={styles.checkBox} type="checkbox" />
           <span className={styles.checkBoxText}>자동 로그인</span>
         </div>
       </div>
-      <button className={styles.loginButton}>로그인</button>
-      <button 
-        className={styles.googleButton}
-        onClick={() => GoogleLoginTest()}
-      >Google로 시작하기</button>
+      {/* <button className={styles.loginButton} onClick={test}>
+        로그인
+      </button> */}
+      <GoogleLogin
+        clientId={clientIDValue}
+        buttonText="구글로 로그인"
+        // onSuccess={googleLogin}
+        onSuccess={() => {
+          window.location.href = "http://localhost:3000/routine";
+        }}
+        onFailure={(res) => console.log(res)}
+        cookiePolicy={"single_host_origin"}
+      />
       <div className={styles.signupForget}>
         <div className={styles.signup}>회원가입</div>
         <div className={styles.forget}>계정 정보를 잊으셨습니까?</div>
       </div>
     </div>
-  )
+  );
 }
-
