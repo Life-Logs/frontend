c53751f0e685847c89747b77b21724b2466b476b
jmlee@ddhinc.net <jmlee@ddhinc.net>
2023-12-28 03:00:28 +0900

    feat: routine page add scroll

--- app/routine/page.tsx
+++ app/routine/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import styles from './page.module.css'
+import styles from './page.module.css';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { BiEdit } from 'react-icons/bi';
@@ -8,239 +8,263 @@ import axios from 'axios';
 import RoutineAdd from '../routineAdd/page';
 
 interface MockData {
-    name: string;
-    type: string;
-    repeat: string;
-    activedAt: string;
-    tag: string;
-  }
-  
+  name: string;
+  type: string;
+  repeat: string;
+  activedAt: string;
+  tag: string;
+}
+
 export default function Routine() {
-    const [fold, setFold] = useState<boolean[]>([])
-    const [data, setData] = useState<MockData[]>([])
-    const [toggleOnOff, setToggleOnOff] = useState<boolean[]>([]);
-    const [modal, setModal] = useState<boolean>(false)
+  const [fold, setFold] = useState<boolean[]>([]);
+  const [data, setData] = useState<MockData[]>([]);
+  const [toggleOnOff, setToggleOnOff] = useState<boolean[]>([]);
+  const [modal, setModal] = useState<boolean>(false);
 
-    const handleToggle = (e: React.MouseEvent<HTMLDivElement>, i: number) => {
-        e.stopPropagation();
-        const dataValue = [...data];
-        // const toggleOnOffValue = [...toggleOnOff]
-        dataValue[i].isActived = !(dataValue[i].isActived);
-        // !toggleOnOffValue[i]
-        axios.patch(`https://lifelog.devtkim.com/routine/${dataValue[i].id}/toggle-activation`, {
-            "isActived": dataValue[i].isActived
-        })
-        .then((response) => {
-            console.log(response)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-        // axios({
-        //     method: 'patch',
-        //     url:`/routine/${dataValue[i].id}/toggle-activation`,
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
+  const handleToggle = (e: React.MouseEvent<HTMLDivElement>, i: number) => {
+    e.stopPropagation();
+    const dataValue = [...data];
+    // const toggleOnOffValue = [...toggleOnOff]
+    dataValue[i].isActived = !dataValue[i].isActived;
+    // !toggleOnOffValue[i]
+    axios
+      .patch(
+        `https://dev.lifelog.devtkim.com/routine/${dataValue[i].id}/toggle-activation`,
+        {
+          isActived: dataValue[i].isActived,
+        }
+      )
+      .then((response) => {
+        // console.log(response)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        // })
-        console.log(dataValue)
-        setData(dataValue)
-        // setToggleOnOff(dataValue);
-    }
+    // console.log(dataValue);
+    setData(dataValue);
+    // setToggleOnOff(dataValue);
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://dev.lifelog.devtkim.com/routine',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        const responseValue = response.data;
+        const toggleArr = [];
+        toggleArr.push(
+          responseValue.map((e) => {
+            return e.isActived ? true : false;
+          })
+        );
+        // const toggleArr = Array(responseValue.length).fill(
+        //     responseValue.map((e) => {
+        //             return e.isActived ? true : false
+        //         })
+        //     );
 
+        setToggleOnOff(toggleArr);
+        // setFold(toggleArr)
+        setFold(Array(responseValue.length).fill(false));
+        setData(responseValue);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    }, [data])
-
-    // useEffect(() => {
-
-    // }, [toggleOnOff])
-
-    useEffect(() => {
-        axios({
-            method: 'get',
-            url: 'https://lifelog.devtkim.com/routine',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((response) => {
-            const responseValue = response.data
-            const toggleArr = []
-            toggleArr.push(responseValue.map((e) => {
-                        return e.isActived ? true : false
-            }))
-            // const toggleArr = Array(responseValue.length).fill(
-            //     responseValue.map((e) => {
-            //             return e.isActived ? true : false
-            //         })
-            //     );
-
-            setToggleOnOff(toggleArr)
-            // setFold(toggleArr)
-            setFold(Array(responseValue.length).fill(false))
-            setData(responseValue)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }, [])
-
-    return (
-        <Body>
-            <WebSize>
-                <RoutineName>루틴</RoutineName>
-                {modal && (
-                    <RoutineAdd setModal={setModal}/>
-                )}
-                {data && data.map((e, i) => (
-                    <RoutineBox routineBoxColor={data[i].isActived} key={i} foldValue={fold[i]}>
-                    <TitleWrap onClick={() => {
-                        const newFold = [...fold]
-                        newFold[i] = !newFold[i]
-                        setFold(newFold)
-                    }}>
-                        <TitleAndToggle >{e.name}
-                        {!fold[i] ?
-                        (
-                            <>
-                                <Tags>{(e.routineTags).map((el) => {
-                                    return '#'+el+' '
-                                })}</Tags>
-                                    <Toggle toggleColor={data[i].isActived} onClick={(e) => handleToggle(e, i)}>
-                                        <ToggleCircle  toggleOnOff={data[i].isActived}/>
-                                    </Toggle>
-                            </>
-                        ) : ''}
-                        </TitleAndToggle>
-                        {fold[i] ? (<BiEdit size="20"/>) : ''}
-                    </TitleWrap>
-                    {fold[i] ? (
-                    <div className={styles.routineBoxFold}>
-                        <div className={styles.routineBoxContentWrap}>
-                            <div className={styles.routineBoxContent}>
-                                <div className={styles.routineMenu}>구분</div>
-                                <div className={styles.routineContent}>{e.type}</div>
-                            </div>
-                            <div className={styles.routineBoxContent}>
-                                <div className={styles.routineMenu}>반복</div>
-                                <div className={styles.routineContent}>{e.repeat}</div>
-                            </div>
-                            <div className={styles.routineBoxContent}>
-                                <div className={styles.routineMenu}>활성</div>
-                                <div className={styles.routineContent}>{e.activedAt}</div>
-                            </div>
-                            <hr className={styles.routineHr}/>
-                            <Tag>{e.tag}</Tag>
+  return (
+    <Body>
+      <WebSize>
+        <RoutineName>루틴</RoutineName>
+        {modal && <RoutineAdd setModal={setModal} />}
+        <RoutineBoxWrapper>
+          {data &&
+            data.map((e, i) => (
+              <RoutineBox
+                routineBoxColor={data[i].isActived}
+                key={i}
+                foldValue={fold[i]}
+              >
+                <TitleWrap
+                  onClick={() => {
+                    const newFold = [...fold];
+                    newFold[i] = !newFold[i];
+                    setFold(newFold);
+                  }}
+                >
+                  <TitleAndToggle>
+                    {e.name}
+                    {!fold[i] ? (
+                      <>
+                        <Tags>
+                          {e.routineTags.map((el) => {
+                            return '#' + el + ' ';
+                          })}
+                        </Tags>
+                        <Toggle
+                          toggleColor={data[i].isActived}
+                          onClick={(e) => handleToggle(e, i)}
+                        >
+                          <ToggleCircle toggleOnOff={data[i].isActived} />
+                        </Toggle>
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </TitleAndToggle>
+                  {fold[i] ? <BiEdit size='20' /> : ''}
+                </TitleWrap>
+                {fold[i] ? (
+                  <div className={styles.routineBoxFold}>
+                    <div className={styles.routineBoxContentWrap}>
+                      <div className={styles.routineBoxContent}>
+                        <div className={styles.routineMenu}>구분</div>
+                        <div className={styles.routineContent}>{e.type}</div>
+                      </div>
+                      <div className={styles.routineBoxContent}>
+                        <div className={styles.routineMenu}>반복</div>
+                        <div className={styles.routineContent}>{e.repeat}</div>
+                      </div>
+                      <div className={styles.routineBoxContent}>
+                        <div className={styles.routineMenu}>활성</div>
+                        <div className={styles.routineContent}>
+                          {e.activedAt}
                         </div>
+                      </div>
+                      <hr className={styles.routineHr} />
+                      <Tag>{e.tag}</Tag>
                     </div>
-                    ) : ''}
-                </RoutineBox>
-                ))}
-                <AddButtonWrapper>
-                    <AddButton onClick={() => {
-                        setModal(!modal)
-                    }}>+</AddButton>
-                </AddButtonWrapper>
-            </WebSize>
-        </Body>
-    )
+                  </div>
+                ) : (
+                  ''
+                )}
+              </RoutineBox>
+            ))}
+        </RoutineBoxWrapper>
+        <AddButtonWrapper>
+          <AddButton
+            onClick={() => {
+              setModal(!modal);
+            }}
+          >
+            +
+          </AddButton>
+        </AddButtonWrapper>
+      </WebSize>
+    </Body>
+  );
 }
 
 const Body = styled.div`
-    display: flex;
-    justify-content: center;
-    background-color: black;
-`
+  display: flex;
+  justify-content: center;
+  background-color: black;
+`;
 
 const WebSize = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    position:relative;
-    min-height: 100vh;
-    background-color: #E2F2D5;
-    width: 390px;    
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  height: 100vh;
+  background-color: #e2f2d5;
+  width: 390px;
+`;
 
 const RoutineName = styled.div`
-    margin-top: 50px;
-`
+  margin-top: 50px;
+`;
 
 const RoutineBox = styled.div`
-    box-shadow: 2px 2px 2px 2px gray;
-    width: 338px;
-    min-height: ${props => (props.foldValue ? '200px' : '60px')}; // fold 값에 따라 동적으로 높이 설정
-    border-radius: 10px;
-    padding: 20px;
-    background-color: ${(props) => props.routineBoxColor ? '#F5FDEE' : '#C5D0BC'};
-    /* background-color: #F5FDEE; */
-    user-select: none;
-    transition: 0.5s;
-    border: none;
-`
+  box-shadow: 3px 3px 3px 3px gray;
+  width: 328px;
+  min-height: ${(props) =>
+    props.foldValue ? '200px' : '60px'}; // fold 값에 따라 동적으로 높이 설정
+  border-radius: 20px;
+  padding: 20px;
+  background-color: ${(props) =>
+    props.routineBoxColor ? '#F5FDEE' : '#C5D0BC'};
+  /* background-color: #F5FDEE; */
+  user-select: none;
+  transition: 0.5s;
+  border: 1px solid gray;
+  margin: 30px 0;
+`;
 const TitleWrap = styled.div`
-    display: flex;
-    justify-content: space-between;
-    cursor: pointer;
-`
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
 
 const Tag = styled.div`
-    color: #64705B;
-    font-size: 12px;
-    font-weight: bold;
-`
+  color: #64705b;
+  font-size: 12px;
+  font-weight: bold;
+`;
 
 const Toggle = styled.div`
-    width: 55px;
-    height: 30px;
-    background-color: ${(props) => props.toggleColor ? '#34C759' : '#64705B'};
-    border-radius: 20px;
-    display:flex;
-    align-items: center;
-    padding: 1px;
-`
+  width: 55px;
+  height: 30px;
+  background-color: ${(props) => (props.toggleColor ? '#34C759' : '#64705B')};
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  padding: 1px;
+`;
 
 const ToggleCircle = styled.div`
-    width: 25px;
-    height: 26px;
-    border-radius: 50px;
-    background-color: white;
-    transform: ${(props) => props.toggleOnOff ? `translateX(27px)` : ''};
-    transition: 0.2s;
-`
+  width: 25px;
+  height: 26px;
+  border-radius: 50px;
+  background-color: white;
+  transform: ${(props) => (props.toggleOnOff ? `translateX(27px)` : '')};
+  transition: 0.2s;
+`;
 
 const TitleAndToggle = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;   
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
 
 const AddButtonWrapper = styled.div`
-    display:flex;
-    justify-content: end;
-    width: 338px;
-    margin-bottom: 20px;
-    /* border: 1px solid red; */
-`
+  display: flex;
+  justify-content: end;
+  width: 338px;
+  margin-bottom: 20px;
+  /* border: 1px solid red; */
+`;
 
 const AddButton = styled.button`
-    width: 50px;
-    height: 50px;
-    border-radius: 50px;
-    border: none;
-    font-size: 32px;
-    font-weight: lighter;
-    background-color: black;
-    color: white;
-    cursor: pointer;
-`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  border: none;
+  font-size: 32px;
+  font-weight: lighter;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+`;
 
 const Tags = styled.p`
-    color: #64705B;
-    font-size: 12px;
-`
+  color: #64705b;
+  font-size: 12px;
+`;
+
+const RoutineBoxWrapper = styled.div`
+  /* border: 1px solid red; */
+  overflow-y: auto;
+  margin: 20px 0;
+  &::-webkit-scrollbar {
+    width: calc(100% - 6px);
+  }
+`;
---
+++ app/routineAdd/DaySelectAndWeekDaySelect.tsx
@@ -0,0 +1,161 @@
+// import styled from "styled-components";
+
+
+// export default function DaySelectAndWeekDaySelect({view, setView, selectWeek, setSelectWeek, choice, setChoice, time, setTime}) {
+
+//     const weekArr = ['매일', '평일', '주말']
+//     const weekDay = ['월', '화', '수', '목', '금', '토', '일']
+
+//     const handleSelectDay = (e:any, i:number) => {
+        
+//         if (e === '평일') {
+//             setChoice([true, true, true, true, true, false, false])
+//         } else if (e === '주말') {
+//             setChoice([false, false, false, false, false, true, true])
+//         } else if (e === '매일') {
+//             setChoice(Array(7).fill(true))
+//         } else if (e === '매주') {
+//             let arr = [...choice]
+//             arr[i] = !arr[i]
+//             if (arr.every((value, index) => value === [true, true, true, true, true, false, false][index])) {
+//                 e = '평일'
+//             } else if (arr.every((value, index) => value === [false, false, false, false, false, true, true][index])) {
+//                 e = '주말'
+//             } else if (arr.every((value, index) => value === [true, true, true, true, true, true, true][index])) {
+//                 e = '매일'
+//             }
+//             setChoice(arr)
+//         }
+//         setSelectWeek(e);        
+
+//     }
+
+//     return (<>
+//             <DaySelectAndWeekDaySelectWrapper>
+//             <DaySelectWrapper>
+//                 <DaySelect onClick={() => setView(!view)}>{selectWeek}<LowIcon>▼</LowIcon></DaySelect>
+//                 {view ?
+//                 (
+//                 <WeekList>
+//                     {weekArr.map((e, i) => {
+//                         return (
+//                         <WeekKind
+//                             key={i}
+//                             onClick={() => {
+//                                 handleSelectDay(e, null);
+//                                 setView(!view);
+//                             }}>{e}
+//                         </WeekKind>
+//                         )
+//                     })}
+//                 </WeekList>
+//                 ) : null}
+//                 <CheckBoxAndText>
+//                     <CheckHoliday type="checkbox"/>
+//                     <HolidayText>공휴일 제외</HolidayText>
+//                 </CheckBoxAndText>
+//             </DaySelectWrapper>
+//             <WeekDaySelectWrapper>
+//                 <Week>
+//                     {weekDay.map((e, i) => {
+//                         return <Day
+//                             key={i}
+//                             choice={choice[i]}
+//                             onClick={() => handleSelectDay('매주', i)}
+//                             > {e}</Day>
+//                     })}
+//                 </Week>
+//                 <Time onClick={() => setTime(!time)}>+ 시간</Time>
+//             </WeekDaySelectWrapper>
+//         </DaySelectAndWeekDaySelectWrapper>
+//         </>)
+// }
+
+// const LowIcon = styled.span`
+//     /* position: absolute;
+//     margin-left: 135px; */
+//     font-size: 12px;
+// `
+
+// const DaySelectWrapper = styled.div`
+//     margin-top: 10px;
+//     background-color: #F6F6F6;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 10px 15px;
+//     user-select: none;
+//     position: relative;
+// `
+// const DaySelect = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     width: 170px;
+//     border-radius: 15px;
+//     border: 1px solid gray;
+//     padding: 8px;
+//     cursor: pointer;
+// `
+
+// const CheckBoxAndText = styled.div`
+//     visibility: hidden;
+//     display: flex;
+//     align-items: center;
+//     user-select:none;
+// `
+
+// const CheckHoliday = styled.input`
+//     cursor: pointer;
+// `
+// const HolidayText = styled.p`
+//     font-size: 14px;
+//     margin-left: 5px;
+// `
+
+// const WeekList = styled.div`
+//     position: absolute;
+//     top: 50px;
+// `
+
+// const WeekKind = styled.div`
+//     background-color: white;
+//     width: 170px;
+//     display: flex;
+//     justify-content: center;
+//     cursor: pointer;
+//     border: 1px solid gray;
+// `
+
+
+// const WeekDaySelectWrapper = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     background-color: #F6F6F6;
+//     padding: 10px 15px;
+// `
+// const Week = styled.div`
+//     width: 220px;
+//     display: flex;
+//     justify-content: space-between;
+// `
+
+// const Day = styled.span`
+//     border: ${(props) => props.choice ? 'black' : 'F6F6F6'} solid 1px;
+//     padding: 2px 4px;
+//     border-radius: 20px;
+//     user-select: none;
+//     cursor: pointer;
+// `
+
+// const Time = styled.button`
+//     border-radius: 20px;
+//     border: none;
+//     background-color: black;
+//     color: white;
+//     font-weight: bold;
+//     padding: 5px;
+//     cursor: pointer;
+// `
+// const DaySelectAndWeekDaySelectWrapper = styled.div``
